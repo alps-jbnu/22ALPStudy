99f88f0b7103006bc8d9bf79cee46997b3c77e64
최인혁 <inhyuk04@gmail.com>
2022-03-10 20:43:41 +0900

    Update 베스트앨범.py

--- Coding_Interview/Code/Sabro98/Hash/베스트앨범.py
+++ Coding_Interview/Code/Sabro98/Hash/베스트앨범.py
@@ -10,15 +10,24 @@ def solution(genres, plays):
         play = plays[i]
 
         if genre in sum_dict.keys():
-            sum_dict[genre] += play
-        else:
+            # 장르의 총 실행 횟수 더함
+            sum_dict[genre] += play 
+        else: # 처음 나오는 장르라면 초기화
             sum_dict[genre] = play
             hash[genre] = []
-
-        hash[genre].append((play, N - i, i))
+        
+        # 해당하는 장르 해시에 음악의 (실행된 수, 실제 위치)
+        hash[genre].append((play, i))
 
     for key in hash.keys():
-        hash[key].sort(reverse=True)
+        # 실행 횟수 -> 1/실제 위치 를 기준으로 역순으로 정렬
+        hash[key].sort(key=lambda x: (x[0], -x[1]), reverse=True)
         array.append((sum_dict[key], key))
     array.sort(reverse=True)
 
+    for _, genre in array:
+        answer.append(hash[genre][0][1])
+        if len(hash[genre]) >= 2:
+            answer.append(hash[genre][1][1])
+
+    return answer
