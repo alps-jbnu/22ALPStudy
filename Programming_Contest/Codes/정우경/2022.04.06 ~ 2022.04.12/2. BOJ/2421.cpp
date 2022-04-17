c0db2507206d3192b78d80dec5c0470f260c2107
Jeong Woo-kyung <77jwk0724@gmail.com>
2022-04-17 23:06:31 +0900

    .

---
+++ Programming_Contest/Codes/정우경/2022.04.06 ~ 2022.04.12/2. BOJ/2421.cpp
@@ -0,0 +1,102 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define EXPAND( x ) x // Use this if MS Visual Studio doesn't expand __VA_ARGS__ correctly
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) EXPAND( GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1) )
+#define FOR(...) EXPAND( F_ORC(__VA_ARGS__ )(__VA_ARGS__) )
+#define EACH(x, a) for (auto& x : a)
+
+const double EPS = 1e-9;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int dy[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+const int dx[] = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+const int MAX = 1e6;
+
+bool isPrime[MAX + 1];
+vector<int> prime(1, 2);
+
+void getPrime() {
+	fill(isPrime + 2, isPrime + MAX + 1, 1);
+
+	for (ll i = 4; i <= MAX; i += 2)
+		isPrime[i] = 0;
+	for (ll i = 3; i <= MAX; i++) {
+		if (isPrime[i]) prime.push_back(i);
+		for (ll j = i * i; j <= MAX; j += i * 2)
+			isPrime[j] = 0;
+	}
+}
+
+int n;
+ll dp[1010][1010];
+
+void init() {
+    FOR(1010) {
+        FOR(j, 1010) {
+            dp[i][j] = -1;
+        }
+    }
+}
+
+void input() {
+    cin >> n;
+}
+
+ll f(int x, int y) {
+    ll& ret = dp[x][y];
+    if (ret != -1) return ret;
+    
+    ret = 0;
+
+    if (x < n) ret = max(ret, f(x + 1, y));
+    if (y < n) ret = max(ret, f(x, y + 1));
+
+    int d = 10;
+    for (; d <= y; d *= 10);
+
+    int idx = x * d + y;
+    if (isPrime[idx]) ret++;
+
+    return ret;
+}
+
+int main() {
+	#ifndef ONLINE_JUDGE
+	// Enter the absolute path of the local file input.txt, output.txt
+	// Or just enter the "input.txt", "output.txt"
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/input.txt", "r", stdin);
+    freopen("/Users/jeongwoo-kyung/Programming/CP-Codes/output.txt", "w", stdout);
+	#endif
+
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    getPrime();
+
+	init();
+
+    input();
+
+    cout << f(1, 1) - 1;
+
+	return 0;
+}
