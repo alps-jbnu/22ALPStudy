7b66b3213bf745cb33473256f6b7e8efbd2bc86e
tlstmdgjs <101104541+tlstmdgjs@users.noreply.github.com>
2022-05-09 16:24:58 +0900

    hi

---
+++ Data_Structure/Code/tlstmdgjs/Recursion/1780.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int val[3]; //-1,0,1로 채워진 종이갯수
+int board[2187][2187];
+
+bool check(int r, int c, int n) {
+	for (int i = r; i < r + n; i++) {
+		for (int j = c; j < c + n; j++) {
+			if (board[r][c] != board[i][j])
+				return false;
+		}
+	}
+	return true;
+}
+
+void divide(int r, int c, int n) {
+	if (check(r, c, n)) {
+		val[board[r][c]]++;
+	}
+	else {
+		int size = n / 3;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				divide(r + size * i, c + size * j, size);
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+			board[i][j]++;
+		}
+	}
+	divide(0, 0, n);
+	cout << val[0] << "\n" << val[1] << "\n" << val[2];
+}
