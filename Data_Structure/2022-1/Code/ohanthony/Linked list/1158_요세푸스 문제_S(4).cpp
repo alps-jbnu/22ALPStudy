8a703c8c00f2aa407da1557aff98178c9afab886
5zbro <90824684+OH-JUHYONG@users.noreply.github.com>
2022-09-19 15:14:53 +0900

    0919

---
+++ Data_Structure/2022-1/Code/ohanthony/Linked list/1158_요세푸스 문제_S(4).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		q.push(i);
+	}
+
+	cout << "<";
+	while (q.size() > 1) {
+		for (int i = 0; i < k-1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front() << ", ";
+		q.pop();
+	}
+	cout << q.front() << ">";
+
+	return 0;
+}
+
