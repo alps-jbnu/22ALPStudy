e462ef7483b682966f7978c29802d08990d10d4a
5zbro <90824684+OH-JUHYONG@users.noreply.github.com>
2022-12-29 02:50:34 +0900

    1228

---
+++ Data_Structure/2022-1/Code/ohanthony/Recursive/1914_하노이 탑_S(1).cpp
@@ -0,0 +1,33 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long // -2^63 ~ 2^63-1
+#define X first
+#define Y second
+
+void hannoi(int st, int en, int n) {
+    if (n == 1) {
+        cout << st << ' ' << en << '\n';
+        return;
+    }
+
+    hannoi(st, 6 - st - en, n - 1);
+    cout << st << ' ' << en << '\n';
+    hannoi(6 - st - en, en, n - 1);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    string s = to_string(pow(2, n)); // long long의 범위는 -2^63 ~ 2^63-1인데 이 문제는 2^100이므로 overflow 문제 발생하므로 다음과 같이 string으로 해결
+    int var = s.find('.'); // pow 함수를 통해 double 형이므로(실수)이 문자로 변환할때 소수 점이 저장되므로 다음 식을 통해 소수 점 찾음
+    s = s.substr(0, var); // 소수 점 앞자리만 저장하게 한다
+    s[s.length() - 1] -= 1; // 2^x - 1이기 때문에 문자의 마지막 index 값에 접근하여 그 값에 -1을 해줌
+    cout << s << '\n';
+
+    if (n <= 20) hannoi(1, 3, n);
+    return 0;
+}
---
+++ Data_Structure/2022-1/Code/ohanthony/Recursive/2630_색종이 만들기_S(2).cpp
@@ -0,0 +1,50 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+#define ll long long // -2^63 ~ 2^63-1
+#define X first
+#define Y second
+
+int board[130][130];
+int blue, white;
+
+void func(int x, int y, int n) {
+    bool isWhite = true;
+    bool isBlue = true;
+
+    for (int i = x; i < x + n; i++) {
+        for (int j = y; j < y + n; j++) {
+            if (board[i][j] == 1) isWhite = false;
+            else isBlue = false;
+        }
+    }
+
+    if (isWhite) {
+        white++;
+        return;
+    }
+    if (isBlue) {
+        blue++;
+        return;
+    }
+
+    func(x, y, n / 2);
+    func(x, y + n / 2, n / 2);
+    func(x + n / 2, y, n / 2);
+    func(x + n / 2, y + n / 2, n / 2);
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int ii = 0; ii < n; ii++) cin >> board[i][ii];
+    }
+
+    func(0, 0, n);
+    cout << white << '\n' << blue;
+    return 0;
+}
